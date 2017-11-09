506ee171aa04805b0ef387188aa10053dc85dbd0
Abed Putra <abedputra@gmail.com>
2017-11-09 10:03:19 +0800

    Update Sql-File.sql

--- Sql-File.sql
+++ Sql-File.sql
@@ -21,20 +21,6 @@ SET time_zone = "+00:00";
 --
 
 -- --------------------------------------------------------
-
---
--- Table structure for table `ci_sessions`
---
-
-CREATE TABLE `ci_sessions` (
-  `id` varchar(40) NOT NULL,
-  `ip_address` varchar(45) NOT NULL,
-  `timestamp` int(10) UNSIGNED NOT NULL DEFAULT '0',
-  `data` blob NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
 --
 -- Table structure for table `tokens`
 --
@@ -45,18 +31,6 @@ CREATE TABLE `tokens` (
   `user_id` int(10) NOT NULL,
   `created` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `tokens`
---
-
-INSERT INTO `tokens` (`id`, `token`, `user_id`, `created`) VALUES
-(57, '7615851ef2e9ddcd71fd45d33534b1', 71, '2017-08-02'),
-(58, '360e03a4084bc54eda53390bbbe11b', 71, '2017-08-02'),
-(59, '1a7384b07e92a3496ff597ae616034', 71, '2017-08-02'),
-(60, 'd569834d3da0595ffa3fb7efd7fa65', 71, '2017-08-02'),
-(61, 'fdbe44a1d6b6891dd9aabde6cd1f38', 71, '2017-08-02'),
-(62, '0834b0af461fd2e707517f8c3343cb', 72, '2017-08-25');
 
 -- --------------------------------------------------------
 
@@ -81,18 +55,11 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `email`, `first_name`, `last_name`, `role`, `password`, `last_login`, `status`, `banned_users`) VALUES
-(71, 'admin@gmail.com', 'Abed', 'Putra', '1', 'sha256:1000:R+nKBWuJFrgqumJqPDY5dIlOwEfdZf0C:nXG9Six8PdLSRSNdcIjzVYJPPbVhJJRa', '2017-08-31 12:54:47 AM', 'approved', 'unban');
+(1, 'admin@gmail.com', 'Abed', 'Putra', '1', 'sha256:1000:R+nKBWuJFrgqumJqPDY5dIlOwEfdZf0C:nXG9Six8PdLSRSNdcIjzVYJPPbVhJJRa', '2017-08-31 12:54:47 AM', 'approved', 'unban');
 
 --
 -- Indexes for dumped tables
 --
-
---
--- Indexes for table `ci_sessions`
---
-ALTER TABLE `ci_sessions`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `ci_sessions_timestamp` (`timestamp`);
 
 --
 -- Indexes for table `tokens`
@@ -114,12 +81,12 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `tokens`
 --
 ALTER TABLE `tokens`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=63;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=73;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
