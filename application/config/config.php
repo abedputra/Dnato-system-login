769d4f75d1b7d04cebd78aa05a385a9a6340771d
sagarppanchal <sagarppanchal.ce@gmail.com>
2019-05-24 11:57:58 +0530

    add mx controller for managing modules

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://adminweb.com/admin/'; //change link
+$config['base_url'] = 'http://172.10.24.37/git/Dnato-system-login/'; //change link
 
 /*
 |--------------------------------------------------------------------------
